c70969091a3901faf428b6eaba9ea05b6fa81e47
Alexander Rice <alexanderrice@rtonational.com>
2025-02-07 13:39:17 -0500

    Revert change to namespace for msmahon/qpdf-php-wrapper

--- src/Facades/Qpdf.php
+++ src/Facades/Qpdf.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Facade as BaseFacade;
  * @method static int getQpdfVersion(string $path)
  * @method static bool fileIsPdf(string $path)
  * @method static int getNumberOfPages(string $path)
- * @method static bool rotate(string $path, \Rton\QpdfPhpWrapper\Enums\Rotation $direction, string $range)
+ * @method static bool rotate(string $path, \Msmahon\QpdfPhpWrapper\Enums\Rotation $direction, string $range)
  * @method static bool trimToRange(string $path, string|int $range)
  * @method static bool combineRangesFromFiles(array $pages, string $output)
  * @method static bool copyPages(string $path, string $outputPath, string $range)
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Facade as BaseFacade;
  * @method static array pageSizes(string $path)
  * @method static void applyStamp(string $path, string $stampPath, string $range = null)
  *
- * @see Rton\QpdfPhpWrapper\Pdf
+ * @see Msmahon\QpdfPhpWrapper\Pdf
  */
 class Qpdf extends BaseFacade
 {
